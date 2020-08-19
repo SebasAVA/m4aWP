67c6440bbada8101e89a66139a824b545b0e8c2e
Sebastian Rojas CASA <js.rojas551@gmail.com>
2020-08-19 12:26:51 -0500

    home test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component'
+import { TeamComponent } from './components/team/team.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'team',
+  component: TeamComponent
+},
+{
+  path: 'home',
+  component: HomeComponent
+},
+{ path: '',   redirectTo: '/home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TeamComponent } from './components/team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
